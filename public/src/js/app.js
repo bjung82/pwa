c63d6b4f7ad338035aa92201649569ee3fe4d52f
Benjamin Jung <bjung@team-jung.de>
2018-01-05 15:47:32 +0100

    Push - client side (subscribe).

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -49,21 +49,47 @@ function displayConfirmNotification() {
 }
 
 function configurePushSub() {
-    if (!('serviceWorker' in navigator)){
-        return;
+    if (!('serviceWorker' in navigator)) {
+      return;
     }
-
+  
+    var reg;
     navigator.serviceWorker.ready
-        .then(function(swreg){
-            return swreg.pushManager.getSubscription();
+      .then(function(swreg) {
+        reg = swreg;
+        return swreg.pushManager.getSubscription();
+      })
+      .then(function(sub) {
+        if (sub === null) {
+          // Create a new subscription
+          var vapidPublicKey = 'BANt59DToCSRB31NjuOUzJRTPLMtVuSUTnj7sNmGIPRGRgBFTgYUuKU2q41fLWnh8FHrh4HpNAeyMgTCszqJ7wk';
+          var convertedVapidPublicKey = urlBase64ToUint8Array(vapidPublicKey);
+          return reg.pushManager.subscribe({
+            userVisibleOnly: true,
+            applicationServerKey: convertedVapidPublicKey
+          });
+        } else {
+          // We have a subscription
+        }
+      })
+      .then(function(newSub) {
+        return fetch('https://pwagram-b105c.firebaseio.com/subscriptions.json', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+          },
+          body: JSON.stringify(newSub)
         })
-        .then(function(sub){
-            if (sub === null) {
-                // create new subscription
-            } else {
-                // We have a subscription
-            }
-        });
+      })
+      .then(function(res) {
+        if (res.ok) {
+          displayConfirmNotification();
+        }
+      })
+      .catch(function(err) {
+        console.log(err);
+    });
 }
 
 function askForNotificationPermission() {
