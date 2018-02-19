6fd5aa3ce581c3264a27d4ef7a7a3db986619bba
Benjamin Jung <bjung@team-jung.de>
2018-02-19 12:58:59 +0100

    Working version with workbax

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,9 +1,15 @@
 importScripts('workbox-sw.prod.v2.1.2.js');
+importScripts('src/js/idb.js');
+importScripts('src/js/utility.js');
 
 const workboxSW = new self.WorkboxSW();
 
 workboxSW.router.registerRoute(/.*(?:googleapis|gstatic)\.com.*$/, workboxSW.strategies.staleWhileRevalidate({
-    cacheName: 'google-fonts'
+    cacheName: 'google-fonts',
+    cacheExpiration: {
+        maxEntries: 3,
+        maxAgeSeconds: 60 * 60 * 24 * 30
+    }
 }));
 
 workboxSW.router.registerRoute('https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css', workboxSW.strategies.staleWhileRevalidate({
@@ -13,6 +19,121 @@ workboxSW.router.registerRoute('https://cdnjs.cloudflare.com/ajax/libs/material-
 workboxSW.router.registerRoute(/.*(?:firebasestorage\.googleapis)\.com.*$/, workboxSW.strategies.staleWhileRevalidate({
     cacheName: 'post-images'
 }));
+
+
+workboxSW.router.registerRoute('https://pwagram-b105c.firebaseio.com/posts.json', function (args) {
+    return fetch(args.event.request)
+        .then(function (res) {
+            var clonedRes = res.clone();
+            clearAllData('posts')
+                .then(function () {
+                    return clonedRes.json();
+                })
+                .then(function (data) {
+                    for (var key in data) {
+                        writeData('posts', data[key])
+                    }
+                });
+            return res;
+        });
+});
+
+self.addEventListener('sync', function(event) {
+    console.log('[Service Worker] Background syncing', event);
+    if (event.tag === 'sync-new-posts') {
+      console.log('[Service Worker] Syncing new Posts');
+      event.waitUntil(
+        readAllData('sync-posts')
+          .then(function(data) {
+            for (var dt of data) {
+              var postData = new FormData();
+              postData.append('id', dt.id);
+              postData.append('title', dt.title);
+              postData.append('location', dt.location);
+              postData.append('rawLocationLat', dt.rawLocation.lat);
+              postData.append('rawLocationLng', dt.rawLocation.lng);
+              postData.append('rawLocationAltitude', dt.rawLocation.altitude);
+              postData.append('file', dt.picture, dt.id + '.png');
+  
+              fetch('https://us-central1-pwagram-b105c.cloudfunctions.net/storePostData', {
+                method: 'POST',
+                body: postData
+              })
+                .then(function(res) {
+                  console.log('Sent data', res);
+                  if (res.ok) {
+                    res.json()
+                      .then(function(resData) {
+                        deleteItemFromData('sync-posts', resData.id);
+                      });
+                  }
+                })
+                .catch(function(err) {
+                  console.log('Error while sending data', err);
+                });
+            }
+  
+          })
+      );
+    }
+  });
+  
+  self.addEventListener('notificationclick', function(event) {
+    var notification = event.notification;
+    var action = event.action;
+  
+    console.log(notification);
+  
+    if (action === 'confirm') {
+      console.log('Confirm was chosen');
+      notification.close();
+    } else {
+      console.log(action);
+      event.waitUntil(
+        clients.matchAll()
+          .then(function(clis) {
+            var client = clis.find(function(c) {
+              return c.visibilityState === 'visible';
+            });
+  
+            if (client !== undefined) {
+              client.navigate(notification.data.url);
+              client.focus();
+            } else {
+              clients.openWindow(notification.data.url);
+            }
+            notification.close();
+          })
+      );
+    }
+  });
+  
+  self.addEventListener('notificationclose', function(event) {
+    console.log('Notification was closed', event);
+  });
+  
+  self.addEventListener('push', function(event) {
+    console.log('Push Notification received', event);
+  
+    var data = {title: 'New!', content: 'Something new happened!', openUrl: '/'};
+  
+    if (event.data) {
+      data = JSON.parse(event.data.text());
+    }
+  
+    var options = {
+      body: data.content,
+      icon: '/src/images/icons/app-icon-96x96.png',
+      badge: '/src/images/icons/app-icon-96x96.png',
+      data: {
+        url: data.openUrl
+      }
+    };
+  
+    event.waitUntil(
+      self.registration.showNotification(data.title, options)
+    );
+  });
 
 
 workboxSW.precache([
@@ -38,7 +159,7 @@ workboxSW.precache([
   },
   {
     "url": "service-worker.js",
-    "revision": "4934fcdacd022183a50baa448c959a97"
+    "revision": "cf728282f759fab351fb703e7c9058e4"
   },
   {
     "url": "src/css/app.css",
@@ -82,7 +203,7 @@ workboxSW.precache([
   },
   {
     "url": "sw-base.js",
-    "revision": "f13d137f97ed6eca9d35215c07512a06"
+    "revision": "7ce55714c779abcd332bab607f5f8a5f"
   },
   {
     "url": "sw.js",
